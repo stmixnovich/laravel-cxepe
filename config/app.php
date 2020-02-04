66c20d8a941bdcd711a444aa547c489940aef087
stmixnovich <st.mixnovich@gmail.com>
2020-02-04 17:04:45 +0300

    Added && configured password

--- config/app.php
+++ config/app.php
@@ -175,6 +175,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /**
+         * OAuth2
+         */
+        Laravel\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('hello', 'API\HelloWorld@show');
+Route::get('hello', 'API\HelloWorld@show')->middleware('auth:api');
